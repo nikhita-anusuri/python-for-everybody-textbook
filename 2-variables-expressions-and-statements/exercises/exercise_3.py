caea6b5899ec62d687537dbd1ece7305f34609c0
Nikhita <nikhi.v.anu@gmail.com>
2025-05-27 09:15:56 -0400

    add try and except block

--- 2-variables-expressions-and-statements/exercises/exercise_3.py
+++ 2-variables-expressions-and-statements/exercises/exercise_3.py
@@ -1,5 +1,8 @@
 # prompts user for a celsius temperature and converts
 # the temp to fahrenheit
-temp_in_c = float(input("Please enter a temperature (celsius): "))
-temp_in_f = (temp_in_c * 1.8) + 32
-print("The temperature in fahrenheit is: " , temp_in_f)
+try:
+    temp_in_c = float(input("Please enter a temperature (celsius): "))
+    temp_in_f = (temp_in_c * 1.8) + 32
+    print("The temperature in fahrenheit is: " , temp_in_f)
+except: 
+    print('Please retry- that is not a number')
