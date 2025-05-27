bd59e49686b84500c6f6581557a3ac090c2211b1
Nikhita <nikhi.v.anu@gmail.com>
2025-05-27 09:13:32 -0400

    edit pay

--- 2-variables-expressions-and-statements/exercises/exercise_1.py
+++ 2-variables-expressions-and-statements/exercises/exercise_1.py
@@ -2,5 +2,5 @@
 
 hours = float(input("Please enter the number of hours you've worked: "))
 rate = float(input("Please enter the hourly pay: "))
-print("Total pay: " + str(hours*rate))
+print("Total pay: " + str(hours*(1.5*rate)))
 
