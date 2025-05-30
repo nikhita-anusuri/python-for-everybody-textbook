743996b20ab22f90102a311c2dcce286f472344a
Nikhita <nikhi.v.anu@gmail.com>
2025-05-29 21:46:05 -0400

    add practice 2 for chapter 4

---
+++ 4-functions/code/practice_2.py
@@ -0,0 +1,21 @@
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics() 
+
+
+print_lyrics()
+repeat_lyrics()
+
+# def is a keyword that comes before the function definition
+# parentheses after function name may contain parameters
+# in this case this function does not take any arguments
+# first line of function def is the header
+# rest of function def is the body
+
+
+
+
