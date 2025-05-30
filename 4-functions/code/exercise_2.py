17e0b10b790e477b3b6d9fabd348aa52a1404d50
Nikhita <nikhi.v.anu@gmail.com>
2025-05-29 21:45:08 -0400

    add exercise 2 for chapter 4

---
+++ 4-functions/code/exercise_2.py
@@ -0,0 +1,6 @@
+# this doesnt work because the function isn't defined before calling it
+print_lyrics()
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
