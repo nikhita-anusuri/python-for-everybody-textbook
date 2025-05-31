627385ac589ec78cbabde0a06654ccc36d8db464
Nikhita <nikhi.v.anu@gmail.com>
2025-05-30 22:50:01 -0400

    add exercise 3 for chapter 4

---
+++ 4-functions/code/exercise_3.py
@@ -0,0 +1,16 @@
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics() 
+
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+
+print_lyrics()
+repeat_lyrics()
+
+# this still works function definitons do not need to be in order
+# function that calls another function can be defined before the other
+# one
