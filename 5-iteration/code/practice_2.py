d9674f183dda8ef4a252e8e97af4840c4fe22c4f
Nikhita <nikhi.v.anu@gmail.com>
2025-06-03 08:02:17 -0400

    add practice 2 to chapter 5

---
+++ 5-iteration/code/practice_2.py
@@ -0,0 +1,9 @@
+count = 0
+for itervar in [3, 41, 12, 74, 15]:
+    count = count + 1
+print('Count: ', count)
+
+total = 0
+for element in [3, 41, 12, 9, 74, 15]:
+    total = total + element
+print('Total: ', total)
