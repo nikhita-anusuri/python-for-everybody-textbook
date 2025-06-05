a48066c6c080f68048337effdd5e4ca5722f5dc7
Nikhita <nikhi.v.anu@gmail.com>
2025-06-04 21:40:34 -0400

    add practice 1 to chapter 6

---
+++ 6-strings/code/practice_1.py
@@ -0,0 +1,11 @@
+# traversal through a string
+fruit = 'banana'
+index = 0
+# while index is less than 6
+while index < len(fruit):
+    # get character at index 
+    letter = fruit[index]
+    # print the character
+    print(letter)
+    # increase index by 1
+    index = index + 1
