8b55c2b6b4536bd1059e7fe979b6d98bf5bbb9a6
Nikhita <nikhi.v.anu@gmail.com>
2025-06-09 15:11:18 -0400

    add practice 3 to chapter 6

---
+++ 6-strings/code/practice_3.py
@@ -0,0 +1,10 @@
+word = 'apple'
+if word < 'banana':
+    print('Your word, ' + word + ', comes before banana.')
+elif word > 'banana':
+    print('Your word, ' + word + ', comes after banana.')
+else:
+    print('All right, bananas.')
+
+# all uppercase letters come before all the lowercase in python:
+# if you do Pineapple with a capital P it'll come before banana
